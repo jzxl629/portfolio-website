58bd60d968e84c8da0fe6a48c6860f7805c7e766
Jack Lu <jackluzhouqian@gmail.com>
2022-07-04 18:08:41 -0700

    Add react icons and style the buttons
    Add linkedin and github icons to the web page.
    Rearrange buttons' positions.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./header.css";
 import CTA from "./CTA";
 import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
@@ -11,11 +12,11 @@ const Header = () => {
         <h1>Zhouqian Lu</h1>
         <h5 className="text-light">Software Developer</h5>
         <CTA />
+        <HeaderSocials />
 
-        {/*
         <div className="me">
-          <img src="{ME}" alt="me" />
-        </div>*/}
+          <img src={ME} alt="me" />
+        </div>
 
         <a href="#contact" className="scroll_down">
           Scroll Down
