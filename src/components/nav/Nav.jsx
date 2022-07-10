576d63b6d57c65a1c794bf71a210820fa15d3a68
Jack Lu <jackluzhouqian@gmail.com>
2022-07-10 01:53:00 -0700

    Add 5 icons to nav bar for each section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineCode } from "react-icons/ai";
+import { RiServiceLine } from "react-icons/ri";
+import { AiOutlineMail } from "react-icons/ai";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <a href="#">
+        <AiOutlineHome />
+      </a>
+      <a href="#about">
+        <AiOutlineUser />
+      </a>
+      <a href="#experience">
+        <AiOutlineCode />
+      </a>
+      <a href="#services">
+        <RiServiceLine />
+      </a>
+      <a href="#contact">
+        <AiOutlineMail />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
