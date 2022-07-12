1c5cc3b346fb13a92fdae5c871275b3111b36fbf
Jack Lu <jackluzhouqian@gmail.com>
2022-07-12 16:01:40 -0700

    Add styles to the experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -13,34 +13,46 @@ const Experience = () => {
           <h3>Frontend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <FiTool />
-              <h4>HTML</h4>
-              <small className="text-light">Intermediate</small>
+              <FiTool className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <FiTool />
-              <h4>CSS</h4>
-              <small className="text-light">Basic</small>
+              <FiTool className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Basic</small>
+              </div>
             </article>
             <article className="experience__details">
-              <FiTool />
-              <h4>JavaScript</h4>
-              <small className="text-light">Basic</small>
+              <FiTool className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Basic</small>
+              </div>
             </article>
             <article className="experience__details">
-              <FiTool />
-              <h4>Bootstrap</h4>
-              <small className="text-light">Intermediate</small>
+              <FiTool className="experience__details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <FiTool />
-              <h4>Django</h4>
-              <small className="text-light">Basic</small>
+              <FiTool className="experience__details-icon" />
+              <div>
+                <h4>Django</h4>
+                <small className="text-light">Basic</small>
+              </div>
             </article>
             <article className="experience__details">
-              <FiTool />
-              <h4>React</h4>
-              <small className="text-light">Basic</small>
+              <FiTool className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Basic</small>
+              </div>
             </article>
           </div>
         </div>
@@ -50,29 +62,39 @@ const Experience = () => {
           <h3>Backend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <FiTool />
-              <h4>Java</h4>
-              <small className="text-light">Intermediate</small>
+              <FiTool className="experience__details-icon" />
+              <div>
+                <h4>Java</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <FiTool />
-              <h4>Python</h4>
-              <small className="text-light">Basic</small>
+              <FiTool className="experience__details-icon" />
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Basic</small>
+              </div>
             </article>
             <article className="experience__details">
-              <FiTool />
-              <h4>C++</h4>
-              <small className="text-light">Basic</small>
+              <FiTool className="experience__details-icon" />
+              <div>
+                <h4>C++</h4>
+                <small className="text-light">Basic</small>
+              </div>
             </article>
             <article className="experience__details">
-              <FiTool />
-              <h4>MySQL</h4>
-              <small className="text-light">Intermediate</small>
+              <FiTool className="experience__details-icon" />
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <FiTool />
-              <h4>Golang</h4>
-              <small className="text-light">Basic</small>
+              <FiTool className="experience__details-icon" />
+              <div>
+                <h4>Golang</h4>
+                <small className="text-light">Basic</small>
+              </div>
             </article>
           </div>
         </div>
